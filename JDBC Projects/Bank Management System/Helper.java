b71bc3a00bf0b9d8ee39b88dacac12663c062ce1
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2023-01-12 00:41:12 +0530

    Add files via upload

---
+++ JDBC Projects/Bank Management System/Helper.java
@@ -0,0 +1,18 @@
+package com.bms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Helper {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection con()throws SQLException{
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+	}
+}
